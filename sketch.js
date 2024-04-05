3ea741296ae52726e5ebaf95bf7f7c8015afeef6
Ancantana <132713654+Ancantana@users.noreply.github.com>
2024-04-04 18:29:30 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ let galleryImages = [];
 let plusButton, downloadButton;
 let selectedImage = null;
 let offsetX, offsetY;
+let draggedImage = null;
+let dragOffsetX, dragOffsetY;
+let imageScaleFactor = 1;
 
 function preload() {
   bgImage = loadImage('AFTERLIFE.png');
@@ -14,18 +17,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   textInput = createInput('');
   textInput.position(width / 2 - textInput.width / 2, height / 2);
-
-  let textColorPicker = createColorPicker('#000000');
-  textColorPicker.position(20, 20);
-  textColorPicker.input(() => {
-    textInput.style('color', textColorPicker.value());
-  });
-
-  let bgColorPicker = createColorPicker('#ffffff');
-  bgColorPicker.position(20, 50);
-  bgColorPicker.input(() => {
-    textInput.style('background-color', bgColorPicker.value());
-  });
 
   plusButton = createImg('plusbutton.png', 'plus button');
   plusButton.position(20, 80);
@@ -44,14 +35,13 @@ function draw() {
   if (bgImage) {
     background(bgImage);
   }
-
   if (galleryVisible) {
     drawGallery();
   }
 }
 
 function initGallery() {
-  let imgUrls = ['original_77f8f96b25a80928f3f31b83d967fd2d.png', 'original_77f8f96b25a80928f3f31b83d967fd2d.png', 'original_77f8f96b25a80928f3f31b83d967fd2d.png'];
+  let imgUrls = ['original\\_77f8f96b25a80928f3f31b83d967fd2d.png', 'original\\_77f8f96b25a80928f3f31b83d967fd2d.png', 'original\\_77f8f96b25a80928f3f31b83d967fd2d.png'];
   imgUrls.forEach(url => {
     let img = createImg(url, 'image');
     img.hide();
@@ -66,42 +56,73 @@ function toggleGallery() {
 function drawGallery() {
   fill(255);
   rect(width - 330, 0, 327, 344);
+
   galleryImages.forEach((img, i) => {
+    let imgX = width - 320;
+    let imgY = i * 110 + 10;
+    let imgW = 100;
+    let imgH = 100;
+
     if (galleryVisible) {
-      img.show();
-      img.position(width - 320, i * 110 + 10);
+      image(img, imgX, imgY, imgW, imgH);
+    }
+
+    // Check if the mouse is over this image
+    if (mouseX >= imgX && mouseX <= imgX + imgW && mouseY >= imgY && mouseY <= imgY + imgH) {
+      cursor(MOVE);
     } else {
-      img.hide();
+      cursor(ARROW);
     }
   });
+
+  // Draw the dragged image on top
+  if (draggedImage) {
+    let imgX = mouseX - dragOffsetX;
+    let imgY = mouseY - dragOffsetY;
+    let imgW = draggedImage.width * imageScaleFactor;
+    let imgH = draggedImage.height * imageScaleFactor;
+    image(draggedImage, imgX, imgY, imgW, imgH);
+  }
 }
 
 function mousePressed() {
   if (dist(mouseX, mouseY, 20, 80) < 15) {
     toggleGallery();
   } else {
-    galleryImages.forEach(img => {
-      let imgX = img.position().x;
-      let imgY = img.position().y;
-      let imgW = img.width;
-      let imgH = img.height;
+    galleryImages.forEach((img, i) => {
+      let imgX = width - 320;
+      let imgY = i * 110 + 10;
+      let imgW = 100;
+      let imgH = 100;
+
       if (mouseX >= imgX && mouseX <= imgX + imgW && mouseY >= imgY && mouseY <= imgY + imgH) {
-        selectedImage = img;
-        offsetX = mouseX - imgX;
-        offsetY = mouseY - imgY;
+        draggedImage = img;
+        dragOffsetX = mouseX - imgX;
+        dragOffsetY = mouseY - imgY;
+
+        // Add a new copy of the image to the gallery
+        let newImg = createImg(img.elt.src, 'image');
+        newImg.hide();
+        galleryImages.push(newImg);
       }
     });
   }
 }
 
 function mouseDragged() {
-  if (selectedImage) {
-    selectedImage.position(mouseX - offsetX, mouseY - offsetY);
+  if (draggedImage) {
+    // Update the position of the dragged image
   }
 }
 
 function mouseReleased() {
-  selectedImage = null;
+  draggedImage = null;
 }
 
-
+function mouseWheel(event) {
+  if (draggedImage) {
+    imageScaleFactor += event.delta * 0.01; // Adjust the scaling factor based on the scroll direction
+    imageScaleFactor = constrain(imageScaleFactor, 0.5, 2); // Constrain the scaling factor to a reasonable range
+  }
+  return false; // Prevent default scrolling behavior
+}
